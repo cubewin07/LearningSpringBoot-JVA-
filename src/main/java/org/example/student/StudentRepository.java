9f9ad5994c58c79f74e79525fc32e5ee4aac6b8c
cubewin <tanthang071208@gmail.com>
2025-07-12 21:27:40 +0700

    Remove redundant code

--- src/main/java/org/example/student/StudentRepository.java
+++ src/main/java/org/example/student/StudentRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    public List<Student> findByFirstName(String firstName);
+     List<Student> findByFirstName(String firstName);
 }
