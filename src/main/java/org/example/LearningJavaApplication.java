f901dc308bee8b429e24d69a8260a9bc78386b9d
cubewin <tanthang071208@gmail.com>
2025-07-12 11:17:14 +0700

    Add new student

--- src/main/java/org/example/LearningJavaApplication.java
+++ src/main/java/org/example/LearningJavaApplication.java
@@ -1,11 +1,21 @@
 package org.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LearningJavaApplication {
     public static void main(String[] args) {
         SpringApplication.run(LearningJavaApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner (StudentRepository studentRepository) {
+        return args -> {
+            Student student = new Student("John", "Doe", "john@gmail.com", 25);
+            studentRepository.save(student);
+        };
     }
 }
