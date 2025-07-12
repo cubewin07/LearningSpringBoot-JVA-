baa4ef3f45fdcad06ebc17fbb831c1686a9a81e4
cubewin <tanthang071208@gmail.com>
2025-07-12 19:56:02 +0700

    Adding simple API

--- src/main/java/org/example/LearningJavaApplication.java
+++ src/main/java/org/example/LearningJavaApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -12,6 +15,12 @@ public class LearningJavaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LearningJavaApplication.class, args);
+
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello world";
     }
 
 }
