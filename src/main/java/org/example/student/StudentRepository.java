52d62d392fc599f6dc342a3989145cb104311567
cubewin <tanthang071208@gmail.com>
2025-07-12 21:08:29 +0700

    Custom method

--- src/main/java/org/example/student/StudentRepository.java
+++ src/main/java/org/example/student/StudentRepository.java
@@ -3,4 +3,5 @@ package org.example.student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    public Student findByFirstName(String firstName);
 }
