4863f1dfccb3f699ef59d16c2531554f2b29f203
cubewin <tanthang071208@gmail.com>
2025-07-12 10:29:09 +0700

    Create new table student

---
+++ src/main/java/org/example/student.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name = "student")
+public class student {
+    @Id
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+
+    public student() {
+    }
+
+    public student(int id, String firstName, String lastName, String email, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
