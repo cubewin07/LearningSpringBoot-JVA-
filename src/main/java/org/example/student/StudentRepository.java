920d63650f77dbf6271efee88591be9a3c110c5f
cubewin <tanthang071208@gmail.com>
2025-07-14 09:23:46 +0700

    Using repo

--- src/main/java/org/example/student/StudentRepository.java
+++ src/main/java/org/example/student/StudentRepository.java
@@ -1,9 +1,11 @@
 package org.example.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
      List<Student> findByFirstName(String firstName);
 }
