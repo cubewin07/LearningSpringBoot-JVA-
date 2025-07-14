f23e63e046b746623d4633eccaa0793a2662678d
cubewin <tanthang071208@gmail.com>
2025-07-14 10:46:46 +0700

    Addng error handling to receive message

---
+++ src/main/java/org/example/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package org.example.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, Object>> handleResponseStatusException(ResponseStatusException ex) {
+        Map<String, Object> body = new HashMap<>();
+        HttpStatus status = HttpStatus.resolve(ex.getStatusCode().value());
+
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", ex.getStatusCode().value());
+        body.put("error", (status != null) ? status.getReasonPhrase() : "Unknown");
+        body.put("message", ex.getReason());
+        return new ResponseEntity<>(body, ex.getStatusCode());
+    }
+}
