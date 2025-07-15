9f6c72ba5315ae926c1c851cdfd7d47c221001f6
cubewin <tanthang071208@gmail.com>
2025-07-15 11:01:44 +0700

    adding setter and gettter

--- src/main/java/org/example/student/StudentRequest.java
+++ src/main/java/org/example/student/StudentRequest.java
@@ -19,4 +19,36 @@ public class StudentRequest {
     @Min(18L)
     @Max(100L)
     private Integer age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
