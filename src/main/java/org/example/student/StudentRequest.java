440fc205c5e1e5b4cb08bef0c093292485a22484
cubewin <tanthang071208@gmail.com>
2025-07-15 10:58:12 +0700

    Create student request - validation

--- src/main/java/org/example/student/StudentRequest.java
+++ src/main/java/org/example/student/StudentRequest.java
@@ -1,9 +1,22 @@
 package org.example.student;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public class StudentRequest {
-    @Email
+    @NotBlank
     private String firstName;
+
+    @NotBlank
     private String lastName;
+
+    @Email
+    @NotBlank
     private String email;
+
+    @Min(18L)
+    @Max(100L)
     private Integer age;
 }
