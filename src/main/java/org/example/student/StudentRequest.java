35526cc7cd3ce4476019ce5df3c47fb7baf6b564
cubewin <tanthang071208@gmail.com>
2025-07-15 10:56:12 +0700

    Adding dependency

---
+++ src/main/java/org/example/student/StudentRequest.java
@@ -0,0 +1,9 @@
+package org.example.student;
+
+public class StudentRequest {
+    @Email
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Integer age;
+}
