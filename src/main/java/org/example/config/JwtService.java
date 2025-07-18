e903064844a33bdba227bf37ead0933ed32f552b
cubewin <tanthang071208@gmail.com>
2025-07-18 11:01:53 +0700

    Create JWT service with extractUsername

--- src/main/java/org/example/config/JwtService.java
+++ src/main/java/org/example/config/JwtService.java
@@ -1,4 +1,37 @@
 package org.example.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.function.Function;
+
 public class JwtService {
+    final static String SECRET_KEY = "ABD9D446AA4D55EC39CBDC5711C45ABD9D446AA4D55EC";
+
+
+    public String extractUsername(String token) {
+
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = SECRET_KEY.getBytes();
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
