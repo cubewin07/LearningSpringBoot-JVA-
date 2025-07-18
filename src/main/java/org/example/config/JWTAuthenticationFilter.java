5b5020aa3271c263aec98d7ba720b0e661464058
cubewin <tanthang071208@gmail.com>
2025-07-18 10:50:48 +0700

    Create JWT authentication file + plain JwtService" " "

--- src/main/java/org/example/config/JWTAuthenticationFilter.java
+++ src/main/java/org/example/config/JWTAuthenticationFilter.java
@@ -1,4 +1,33 @@
 package org.example.config;
 
-public class JWTAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authenHeader = request.getHeader("Authentication");
+        final String jwt;
+        final String username;
+        if(authenHeader == null || !authenHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+        }
+        jwt = authenHeader.substring(7);
+        username = jwtService.extractUsername(jwt);
+    }
 }
