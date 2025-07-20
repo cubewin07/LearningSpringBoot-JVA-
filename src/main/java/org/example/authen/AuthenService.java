4a7608734b4c7ad1285ab389764c9fc45f318f3f
cubewin <tanthang071208@gmail.com>
2025-07-20 15:24:52 +0700

    Create authenticateUser service

--- src/main/java/org/example/authen/AuthenService.java
+++ src/main/java/org/example/authen/AuthenService.java
@@ -5,6 +5,8 @@ import org.example.config.JwtService;
 import org.example.user.Role;
 import org.example.user.User;
 import org.example.user.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,7 @@ public class AuthenService {
     private final UserRepository userRepository;
     private final JwtService jwtService;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse registerUser(RegisterRequest request) {
         var user = User.builder()
@@ -27,5 +30,19 @@ public class AuthenService {
                 .token(jwtToken)
                 .build();
 
+    }
+
+    public AuthenticationResponse authenticateUser( AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.email(),
+                        request.password()
+                )
+        );
+        var user = userRepository.findByEmail(request.email());
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
