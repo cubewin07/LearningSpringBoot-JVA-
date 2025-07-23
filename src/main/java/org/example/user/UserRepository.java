7104d884f7fb329833d2df38bb164ecedc2a010e
cubewin <tanthang071208@gmail.com>
2025-07-23 09:48:14 +0700

    Adding new method to check user email

--- src/main/java/org/example/user/UserRepository.java
+++ src/main/java/org/example/user/UserRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<UserDetails> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
