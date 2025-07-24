0962ff8832fa2a5f3db1460f97609493b2746885
cubewin <tanthang071208@gmail.com>
2025-07-24 10:37:32 +0700

    tried but failed to test due to mocking issue with jwtService

--- src/test/java/org/example/authen/AuthenServiceTest.java
+++ src/test/java/org/example/authen/AuthenServiceTest.java
@@ -9,17 +9,22 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthenServiceTest {
 
     @Mock
     private UserRepository userRepository;
+
+//    @Spy
+//    private JwtService jwtService = Mockito.spy(new JwtService());
 
     @Mock
     private JwtService jwtService;
@@ -34,6 +39,9 @@ public class AuthenServiceTest {
 
         var user = User.builder().name(request.name()).email(request.email()).password(request.password()).role(Role.USER).build();
 
+
+
+//        doReturn("token").when(jwtService).generateToken(user);
         Mockito.when(jwtService.generateToken(user)).thenReturn("mock-token");
 
         String token = authenService.registerUser(request).getToken();
