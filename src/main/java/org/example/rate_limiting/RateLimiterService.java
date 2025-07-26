1e76f2e7b47cc42b24b6d063d4f51d8de232c735
cubewin <tanthang071208@gmail.com>
2025-07-26 10:03:08 +0700

    Create ratelimiterService

--- src/main/java/org/example/rate_limiting/RateLimiterService.java
+++ src/main/java/org/example/rate_limiting/RateLimiterService.java
@@ -1,4 +1,26 @@
 package org.example.rate_limiting;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@Component
 public class RateLimiterService {
+    private final Map<String, Bucket> cache = new ConcurrentHashMap<>();
+
+    public Bucket resolveBucket(String key) {
+        return cache.computeIfAbsent(key, this::newBucket);
+    }
+
+    public Bucket newBucket(String key) {
+        return Bucket.builder()
+                .addLimit(limit -> limit.capacity(10).refillIntervally(10, Duration.ofMinutes(1)))
+                .build();
+    }
+
 }
