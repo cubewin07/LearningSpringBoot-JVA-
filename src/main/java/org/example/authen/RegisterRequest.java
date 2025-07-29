12b1fb086a09f0227d911ddba045fad9c8ae3458
cubewin <tanthang071208@gmail.com>
2025-07-29 11:36:17 +0700

    Validate request

--- src/main/java/org/example/authen/RegisterRequest.java
+++ src/main/java/org/example/authen/RegisterRequest.java
@@ -1,7 +1,18 @@
 package org.example.authen;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
-public record RegisterRequest(String name, String password, String email) {
-}
+public record RegisterRequest(
+
+        @NotNull(message = "Name can't be null")
+        String name,
+        @Size(min = 6, message = "Password must be at least 6 characters long")
+        String password,
+        @Email(message = "Email must be valid")
+        String email
+)
+{}
