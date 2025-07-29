6e7166b30f9125b421b8fcdd2b866a173cba6fd0
cubewin <tanthang071208@gmail.com>
2025-07-29 11:37:23 +0700

    Do the same for authentication request

--- src/main/java/org/example/authen/AuthenticationRequest.java
+++ src/main/java/org/example/authen/AuthenticationRequest.java
@@ -1,4 +1,12 @@
 package org.example.authen;
 
-public record AuthenticationRequest(String email, String password) {
-}
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record AuthenticationRequest(
+        @NotNull(message = "Email can't be null")
+        String email,
+        @Size(min = 6, message = "Password must be at least 6 characters long")
+        String password
+)
+{}
