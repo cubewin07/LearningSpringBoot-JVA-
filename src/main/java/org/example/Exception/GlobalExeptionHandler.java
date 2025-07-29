e8297007b252662ae8481df6294cf8e2b23107d0
cubewin <tanthang071208@gmail.com>
2025-07-29 11:43:41 +0700

    Map through each failed validation to get their message

--- src/main/java/org/example/Exception/GlobalExeptionHandler.java
+++ src/main/java/org/example/Exception/GlobalExeptionHandler.java
@@ -2,11 +2,14 @@ package org.example.Exception;
 
 
 import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
 
 @ControllerAdvice
 public class GlobalExeptionHandler {
@@ -55,9 +58,10 @@ public class GlobalExeptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorRes> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).toList();
         ErrorRes error = new ErrorRes(
                 HttpStatus.BAD_REQUEST.value(),
-                ex.getMessage(),
+                errors,
                 System.currentTimeMillis()
         );
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
