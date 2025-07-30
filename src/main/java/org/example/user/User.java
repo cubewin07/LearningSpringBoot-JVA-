069b385fd599005a152343cbbaeaceb63b61f4a6
cubewin <tanthang071208@gmail.com>
2025-07-30 09:20:40 +0700

    Using many to many relationship in user

--- src/main/java/org/example/user/User.java
+++ src/main/java/org/example/user/User.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -38,6 +39,8 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @ManyToMany(fetch = FetchType.LAZY)
+    private List<Course> courses = new ArrayList<>();
 
     @Override
     public String getUsername() {
