c83d68379742a9f2f5ec657572d8ee963d39ad94
cubewin <tanthang071208@gmail.com>
2025-07-30 15:10:16 +0700

    Using fetch join for course repository

--- src/main/java/org/example/course/CourseRepository.java
+++ src/main/java/org/example/course/CourseRepository.java
@@ -1,6 +1,13 @@
 package org.example.course;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    @Query("SELECT c FROM Course c LEFT JOIN FETCH c.users WHERE c.id = :id")
+    Optional<Course> findCourseByIdWithUser(@Param("id") Long id);
 }
