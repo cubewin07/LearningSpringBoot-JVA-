c6239021ddac486726c32c506324403ee25d2f2f
cubewin <tanthang071208@gmail.com>
2025-07-30 11:17:47 +0700

    Using join fetch to prevent lazy load

--- src/main/java/org/example/user/UserRepository.java
+++ src/main/java/org/example/user/UserRepository.java
@@ -1,11 +1,16 @@
 package org.example.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<UserDetails> findByEmail(String email);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.courses where u.email = :email")
+    Optional<User> findByEmailWithCourses(@Param("email") String email);
     Boolean existsByEmail(String email);
 }
