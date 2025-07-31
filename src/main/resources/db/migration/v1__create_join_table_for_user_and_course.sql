2de179e891f94f5d7b38262fc6110a287da22058
cubewin <tanthang071208@gmail.com>
2025-07-31 19:05:41 +0700

    Create join table for user and course

---
+++ src/main/resources/db/migration/v1__create_join_table_for_user_and_course.sql
@@ -0,0 +1,7 @@
+create table users_courses (
+    user_id BIGINT NOT NULL,
+    course_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, course_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (course_id) REFERENCES course(id)
+)
