84656d5be3230b9542144502f3d650bc15454b12
cubewin <tanthang071208@gmail.com>
2025-08-02 10:23:57 +0700

    Enable caching

--- src/main/java/org/example/LearningJavaApplication.java
+++ src/main/java/org/example/LearningJavaApplication.java
@@ -3,6 +3,7 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @RestController
+@EnableCaching
 public class LearningJavaApplication {
 
     public static void main(String[] args) {
