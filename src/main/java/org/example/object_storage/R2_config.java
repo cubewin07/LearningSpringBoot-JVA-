f6ad903d47c6fd3953e2625276d3fb318f50ba2c
cubewin <tanthang071208@gmail.com>
2025-08-09 20:24:21 +0700

    Create r2 client

--- src/main/java/org/example/object_storage/R2_config.java
+++ src/main/java/org/example/object_storage/R2_config.java
@@ -3,6 +3,14 @@ package org.example.object_storage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.identity.spi.AwsCredentialsIdentity;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
 
 @Configuration
 public class R2_config {
@@ -16,4 +24,15 @@ public class R2_config {
     @Value("${CLOUDFLARE_R2_ENDPOINT_URL}")
     private String endpoint;
 
+    @Bean
+   public S3Client r2Client() {
+        AwsCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(Region.of("auto"))
+                .credentialsProvider(() -> credentials)
+                .build();
+
+    }
 }
