a9bca69c1499cc05621d39138d02e6fa967c2257
cubewin <tanthang071208@gmail.com>
2025-08-12 20:32:34 +0700

    Init firebase admin SDK

---
+++ src/main/java/org/example/firebase_cloud_messaging/FirebaseInitializer.java
@@ -0,0 +1,27 @@
+package org.example.firebase_cloud_messaging;
+
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import jakarta.annotation.PostConstruct;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirebaseInitializer {
+
+    @PostConstruct
+    public void init() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("/Users/letanthang/learning\\ software/firebaseServiceAccountKey.json");
+
+       FirebaseOptions options = FirebaseOptions.builder()
+               .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+               .build();
+
+       FirebaseApp.initializeApp(options);
+    }
+}
