e2b63ca262b67c5f43eff9fceff833e9088fb239
cubewin <tanthang071208@gmail.com>
2025-08-13 15:16:48 +0700

    set system properties before run application

--- src/main/java/org/example/LearningJavaApplication.java
+++ src/main/java/org/example/LearningJavaApplication.java
@@ -1,6 +1,7 @@
 
 package org.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -15,6 +16,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LearningJavaApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .filename(".env.local")
+                .ignoreIfMissing()
+                .load();
+
+        System.setProperty("CLOUDFLARE_R2_ACCESS_KEY_ID", dotenv.get("CLOUDFLARE_R2_ACCESS_KEY_ID"));
+        System.setProperty("CLOUDFLARE_R2_SECRET_ACCESS_KEY", dotenv.get("CLOUDFLARE_R2_SECRET_ACCESS_KEY"));
+        System.setProperty("CLOUDFLARE_R2_ENDPOINT_URL", dotenv.get("CLOUDFLARE_R2_ENDPOINT_URL"));
         SpringApplication.run(LearningJavaApplication.class, args);
     }
 }
