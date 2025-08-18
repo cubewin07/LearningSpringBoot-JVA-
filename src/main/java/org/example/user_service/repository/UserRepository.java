efe25f52f24d75a5d6afc7ff702a61c7474d53fd
cubewin <tanthang071208@gmail.com>
2025-08-18 14:23:24 +0700

    Using entity graph

--- src/main/java/org/example/user_service/repository/UserRepository.java
+++ src/main/java/org/example/user_service/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package org.example.user_service.repository;
 
 import org.example.user_service.model.User;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -10,6 +14,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @EntityGraph(attributePaths = "courses")
     Optional<User> findByEmail(String email);
 
+    @EntityGraph(attributePaths = "courses")
+    <S extends User> Page<S> findAll(Example<S> example, Pageable pageable);
 
 //    @EntityGraph(attributePaths = "courses")
 //    Optional<User> findByEmailWithCourses(@Param("email") String email);
