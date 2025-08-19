e3fd56f00b4510c2cb3cbfd31c8e02036eba16fd
cubewin <tanthang071208@gmail.com>
2025-08-19 19:28:49 +0700

    Giving hint for tomorrow work

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -44,6 +44,10 @@ public class SecurityConfig {
                                 .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
+                .oauth2Login(oauth2 ->
+                        oauth2
+                                .successHandler(myAuthSuccessHanlder)
+                )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider(userDetailsService))
                 .addFilterBefore(JWTAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
