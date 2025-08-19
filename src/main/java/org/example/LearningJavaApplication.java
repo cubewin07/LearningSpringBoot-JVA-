94c87e1b0c639d62964ba6508176dce32c8d29cd
cubewin <tanthang071208@gmail.com>
2025-08-19 08:56:12 +0700

    create environment variables

--- src/main/java/org/example/LearningJavaApplication.java
+++ src/main/java/org/example/LearningJavaApplication.java
@@ -25,6 +25,11 @@ public class LearningJavaApplication {
         System.setProperty("CLOUDFLARE_R2_SECRET_ACCESS_KEY", dotenv.get("CLOUDFLARE_R2_SECRET_ACCESS_KEY"));
         System.setProperty("CLOUDFLARE_R2_ENDPOINT_URL", dotenv.get("CLOUDFLARE_R2_ENDPOINT_URL"));
         System.setProperty("CLOUDINARY_URL", dotenv.get("CLOUDINARY_URL"));
+
+        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+        System.setProperty("GITHUB_CLIENT_ID", dotenv.get("GITHUB_CLIENT_ID"));
+        System.setProperty("GITHUB_CLIENT_SECRET", dotenv.get("GITHUB_CLIENT_SECRET"));
         SpringApplication.run(LearningJavaApplication.class, args);
     }
 }
