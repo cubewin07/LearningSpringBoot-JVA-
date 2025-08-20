ffd088bb4a11eaaa1a9b6201df1612838471224d
cubewin <tanthang071208@gmail.com>
2025-08-20 20:05:04 +0700

    Return jwt json object

--- src/main/java/org/example/oauth/MyAuthSuccessHandler.java
+++ src/main/java/org/example/oauth/MyAuthSuccessHandler.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.example.config.JwtService;
+import org.example.user_service.model.Role;
+import org.example.user_service.model.User;
+import org.example.user_service.repository.UserRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -17,6 +20,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class MyAuthSuccessHandler implements AuthenticationSuccessHandler {
     private final JwtService jwtService;
+    private final UserRepository userRepository;
 
     @Override
     public void onAuthenticationSuccess(
@@ -24,6 +28,27 @@ public class MyAuthSuccessHandler implements AuthenticationSuccessHandler {
             HttpServletResponse response,
             Authentication authentication
     ) throws IOException, ServletException {
-        OAuth2User
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+
+        String email = oAuth2User.getAttribute("email");
+        String username = oAuth2User.getAttribute("name");
+        String picture = oAuth2User.getAttribute("picture");
+
+        if(userRepository.findByEmail(email).isEmpty()) {
+            User user = User.builder()
+                    .name(username)
+                    .email(email)
+                    .role(Role.USER)
+                    .build();
+            userRepository.save(user);
+        }
+
+        String jwt = jwtService.generateToken(email);
+
+        // Send JWT as a JSON response
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write("{\"token\": \"" + jwt + "\"}");
+        response.getWriter().flush();
     }
 }
