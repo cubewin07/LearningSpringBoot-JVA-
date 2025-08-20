1cb9316cff4d1d03660daef7b8a1f2f236b74fff
cubewin <tanthang071208@gmail.com>
2025-08-20 18:36:58 +0700

    init success handler

---
+++ src/main/java/org/example/oauth/MyAuthSuccessHandler.java
@@ -0,0 +1,23 @@
+package org.example.oauth;
+
+import com.google.auth.oauth2.OAuth2CredentialsWithRefresh;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class MyAuthSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication
+    ) throws IOException, ServletException {
+
+    }
+}
