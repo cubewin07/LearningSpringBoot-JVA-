40b052e68bc2a9d6f5945e1a146b6a7b35ce971b
cubewin <tanthang071208@gmail.com>
2025-08-20 18:42:19 +0700

    Inject jwt service

--- src/main/java/org/example/oauth/MyAuthSuccessHandler.java
+++ src/main/java/org/example/oauth/MyAuthSuccessHandler.java
@@ -4,6 +4,8 @@ import com.google.auth.oauth2.OAuth2CredentialsWithRefresh;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.example.config.JwtService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -11,7 +13,10 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class MyAuthSuccessHandler implements AuthenticationSuccessHandler {
+    private final JwtService jwtService;
+
     @Override
     public void onAuthenticationSuccess(
             HttpServletRequest request,
