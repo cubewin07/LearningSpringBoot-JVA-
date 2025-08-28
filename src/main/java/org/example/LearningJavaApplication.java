3df4f41f18d347699a8182c9a7c4eb2c950459b9
cubewin <tanthang071208@gmail.com>
2025-08-28 09:25:46 +0700

    Making docker-compose

--- src/main/java/org/example/LearningJavaApplication.java
+++ src/main/java/org/example/LearningJavaApplication.java
@@ -16,20 +16,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LearningJavaApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure()
-                .filename(".env.local")
-                .ignoreIfMissing()
-                .load();
+        // Dotenv dotenv = Dotenv.configure()
+        //         .filename(".env.local")
+        //         .ignoreIfMissing()
+        //         .load();
 
-        System.setProperty("CLOUDFLARE_R2_ACCESS_KEY_ID", dotenv.get("CLOUDFLARE_R2_ACCESS_KEY_ID"));
-        System.setProperty("CLOUDFLARE_R2_SECRET_ACCESS_KEY", dotenv.get("CLOUDFLARE_R2_SECRET_ACCESS_KEY"));
-        System.setProperty("CLOUDFLARE_R2_ENDPOINT_URL", dotenv.get("CLOUDFLARE_R2_ENDPOINT_URL"));
-        System.setProperty("CLOUDINARY_URL", dotenv.get("CLOUDINARY_URL"));
+        // System.setProperty("CLOUDFLARE_R2_ACCESS_KEY_ID", dotenv.get("CLOUDFLARE_R2_ACCESS_KEY_ID"));
+        // System.setProperty("CLOUDFLARE_R2_SECRET_ACCESS_KEY", dotenv.get("CLOUDFLARE_R2_SECRET_ACCESS_KEY"));
+        // System.setProperty("CLOUDFLARE_R2_ENDPOINT_URL", dotenv.get("CLOUDFLARE_R2_ENDPOINT_URL"));
+        // System.setProperty("CLOUDINARY_URL", dotenv.get("CLOUDINARY_URL"));
 
-        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
-        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
-        System.setProperty("GITHUB_CLIENT_ID", dotenv.get("GITHUB_CLIENT_ID"));
-        System.setProperty("GITHUB_CLIENT_SECRET", dotenv.get("GITHUB_CLIENT_SECRET"));
+        // System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        // System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+        // System.setProperty("GITHUB_CLIENT_ID", dotenv.get("GITHUB_CLIENT_ID"));
+        // System.setProperty("GITHUB_CLIENT_SECRET", dotenv.get("GITHUB_CLIENT_SECRET"));
         SpringApplication.run(LearningJavaApplication.class, args);
     }
 }
