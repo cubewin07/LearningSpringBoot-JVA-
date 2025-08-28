65a98b846253e06d61673a5204f3178f4b2b33e1
cubewin <tanthang071208@gmail.com>
2025-08-28 15:14:52 +0700

    Debug

--- src/main/java/org/example/notification/firebase_cloud_messaging/FirebaseInitializer.java
+++ src/main/java/org/example/notification/firebase_cloud_messaging/FirebaseInitializer.java
@@ -9,19 +9,35 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.stereotype.Component;
 
+class FirebaseEnabledCondition implements Condition {
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        String enabled = context.getEnvironment().getProperty("spring.firebase.enabled", "false");
+        return Boolean.parseBoolean(enabled);
+    }
+}
+
 @Component
+@Conditional(FirebaseEnabledCondition.class)
 public class FirebaseInitializer {
+
+    @Value("${firebase.credentials.path:/app/firebaseServiceAccountKey_messaging.json}")
+    private String credentialsPath;
 
     @PostConstruct
     public void init() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("/Users/letanthang/learning_software/firebaseServiceAccountKey_messaging.json");
-
-       FirebaseOptions options = FirebaseOptions.builder()
-               .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-               .build();
-
-       FirebaseApp.initializeApp(options);
+        try (FileInputStream serviceAccount = new FileInputStream(credentialsPath)) {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+            FirebaseApp.initializeApp(options);
+        }
     }
 }
