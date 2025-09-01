153a9daeaf83d64544cfea7cec35f4ca396702cf
cubewin <tanthang071208@gmail.com>
2025-09-01 21:06:08 +0700

    Manage file

--- src/main/java/org/example/file_uploading_service/config/R2_config.java
+++ src/main/java/org/example/file_uploading_service/config/R2_config.java
@@ -1,12 +1,10 @@
-package org.example.file_uploading_service.object_storage;
+package org.example.file_uploading_service.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.identity.spi.AwsCredentialsIdentity;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
